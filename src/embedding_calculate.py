86c6927345c57aeca5ea15c49334300d4dbec462
gavrilenkoa <gavrilenkoalex154@gmail.com>
2024-03-11 12:58:01 +0300

    add embedding calculation

--- src/embedding_calculate.py
+++ src/embedding_calculate.py
@@ -4,7 +4,7 @@ import torch
 import pandas as pd
 import pickle
 import os
-from transformers import AutoTokenizer, EsmModel
+from transformers import AutoTokenizer, EsmModel, T5EncoderModel, T5Tokenizer
 import logging
 
 
@@ -13,27 +13,52 @@ def save_embeds(obj: object, data_name: str, model_name: str) -> None:
         filename = f"../data/embeddings/esm_embeddings/{data_name}.pkl"
     elif model_name == "prot5":
         filename = f"../data/embeddings/prot5_embeddings/{data_name}.pkl"
+    elif model_name == "ankh":
+        filename = f"../data/embeddings/ankh_embeddings/{data_name}.pkl"
 
     with open(filename, "wb") as file:
         pickle.dump(obj, file)
 
 
 def calculate_embed(tokenizer, model, seq: str, model_name: str) -> np.ndarray:
-    with torch.no_grad():
-        if model_name == "ankh":
-            inputs = tokenizer(
-                [seq],
-                add_special_tokens=False,
-                padding=False,
-                is_split_into_words=True,
-                return_tensors="pt")
+    if model_name == "ankh":
+        inputs = tokenizer(
+            [seq],
+            add_special_tokens=False,
+            padding=False,
+            is_split_into_words=True,
+            return_tensors="pt")
 
-        elif model_name == "esm":
-            inputs = tokenizer(seq, return_tensors="pt")
+        with torch.no_grad():
+            inputs.to(torch.device("cuda"))
+            outputs = model(**inputs)
+            embedding = outputs.last_hidden_state.mean(axis=1).view(-1).cpu().numpy()
 
-    inputs.to(torch.device("cuda"))
-    outputs = model(**inputs)
-    embedding = outputs.last_hidden_state.mean(axis=1).view(-1).cpu().numpy()
+    elif model_name == "esm":
+        inputs = tokenizer(seq, return_tensors="pt")
+
+        with torch.no_grad():
+            inputs.to(torch.device("cuda"))
+            outputs = model(**inputs)
+            embedding = outputs.last_hidden_state.mean(axis=1).view(-1).cpu().numpy()
+
+    elif model_name == "prot5":
+        item = []
+        for i in range(len(seq)):
+            if i != 0 and i != len(seq):
+                item.append(" ")
+            item.append(seq[i])
+
+        item = ["".join(item)]
+
+        ids = tokenizer.batch_encode_plus(item, add_special_tokens=False, padding=False)
+        input_ids = torch.tensor(ids['input_ids']).to(torch.device("cuda"))
+        attention_mask = torch.tensor(ids['attention_mask']).to(torch.device("cuda"))
+
+        with torch.no_grad():
+            embedding = model(input_ids=input_ids, attention_mask=attention_mask)
+            embedding = embedding.last_hidden_state.mean(axis=1).view(-1).cpu().numpy()
+
     return embedding
 
 
@@ -69,7 +94,7 @@ def main():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
-        filename=f"{model_name}.log",
+        filename=f"{input_csv}_{model_name}.log",
     )
 
     if model_name == "ankh":
@@ -78,6 +103,10 @@ def main():
     elif model_name == "esm":
         tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t48_15B_UR50D")
         model = EsmModel.from_pretrained("facebook/esm2_t48_15B_UR50D")
+
+    elif model_name == "prot5":
+        tokenizer = T5Tokenizer.from_pretrained("Rostlab/prot_t5_xl_uniref50", do_lower_case=False)
+        model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_uniref50")
 
     process_data(tokenizer, model, input_csv, model_name)
 
