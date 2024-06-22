f56340f29ec676f1e826b588d63906f5e2cef4bb
gavrilenkoa <gavrilenkoalex154@gmail.com>
2024-06-22 17:51:43 +0300

    optuna_search

---
+++ train-code/data_prepare.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import GroupKFold
+import h5py
+
+
+def load_dict_from_hdf5(filename):
+    """
+    Load a dictionary with string keys and NumPy array values from an HDF5 file.
+
+    Parameters:
+    filename (str): Name of the HDF5 file to load the data from.
+
+    Returns:
+    dict: Dictionary with string keys and NumPy array values.
+    """
+    loaded_dict = {}
+    with h5py.File(filename, "r") as f:
+        for key in f.keys():
+            loaded_dict[key] = f[key][:]
+    return loaded_dict
+
+
+def prepare_folds(
+    embedding_path="../../../../ssd2/dbp_finder/ankh_embeddings/train_p2_2d.h5",
+    train_data_path="../data/ready_data/train_pdb2272.csv",
+    n_splits=5,
+):
+    # Load embeddings and process them
+    embeddings = load_dict_from_hdf5(embedding_path)
+    for key in embeddings:
+        embeddings[key] = np.squeeze(embeddings[key])
+
+    embed_df = pd.DataFrame(
+        list(embeddings.items()), columns=["identifier", "embedding"]
+    )
+
+    # Load training data and merge with embeddings
+    train_df = pd.read_csv(train_data_path)
+    train_df = train_df.merge(embed_df, on="identifier")
+
+    # Prepare data for GroupKFold
+    X = train_df["sequence"].tolist()
+    y = train_df["label"].tolist()
+    groups = train_df["cluster"].tolist()
+    gkf = GroupKFold(n_splits=n_splits)
+
+    # Split data into training and validation folds
+    train_folds = []
+    valid_folds = []
+
+    for train_idx, valid_idx in gkf.split(X, y, groups=groups):
+        train_idx = train_idx.tolist()
+        valid_idx = valid_idx.tolist()
+
+        train = train_df.iloc[train_idx]
+        valid = train_df.iloc[valid_idx]
+
+        train_folds.append(train)
+        valid_folds.append(valid)
+
+    return train_folds[1], valid_folds[1]
--- train-code/optuna.py
+++ train-code/optuna.py
@@ -1,7 +1,98 @@
+import ankh
 import torch
+import optuna
+from torch.optim import AdamW
+from torch.utils.data import DataLoader
+import clearml
+import logging
+from clearml import Logger, Task
+from torch_utils import (
+    SequenceDataset,
+    CustomBatchSampler,
+    custom_collate_fn,
+    train_fn,
+    validate_fn,
+)
+from data_prepare import prepare_folds
 
 
+train, valid = prepare_folds()
 
 
 def objective(trial):
-    DEVICE = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+    DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    batch_size = trial.suggest_categorical("batch_size", [32, 64, 128])
+    lr = trial.suggest_loguniform("lr", 1e-5, 1e-1)
+    pooling = trial.suggest_categorical("pooling", ["max", "avg"])
+    hidden_dim = trial.suggest_int("hidden_dim", 1536, 1800)
+    kernel_size = trial.suggest_int("kernel_size", 5, 9)
+    dropout = trial.suggest_float("dropout", 0.0, 0.3, step=0.1)
+    num_hidden_layers = trial.num_hidden_layers("num_hidden_layers", 1, 3)
+    num_layers = trial.suggest_int("num_layers", 1, 3)
+    nhead = trial.suggest_int("nhead", 2, 6)
+
+    binary_classification_model = ankh.ConvBertForBinaryClassification(
+        input_dim=1536,
+        nhead=nhead,
+        hidden_dim=hidden_dim,
+        num_hidden_layers=num_hidden_layers,
+        num_layers=num_layers,
+        kernel_size=kernel_size,
+        dropout=dropout,
+        pooling=pooling,
+    )
+
+    optimizer = AdamW(binary_classification_model.parameters(), lr=lr)
+    train_dataset = SequenceDataset(train)
+    train_sampler = CustomBatchSampler(train_dataset, batch_size)
+    train_dataloader = DataLoader(
+        train_dataset,
+        num_workers=4,
+        batch_sampler=train_sampler,
+        collate_fn=custom_collate_fn,
+    )
+
+    valid_dataset = SequenceDataset(valid)
+    valid_sampler = CustomBatchSampler(valid_dataset, batch_size)
+    valid_dataloader = DataLoader(
+        valid_dataset,
+        num_workers=4,
+        batch_sampler=valid_sampler,
+        collate_fn=custom_collate_fn,
+    )
+
+    best_val_loss = float("inf")
+    for _ in range(15):
+        _ = train_fn(binary_classification_model, train_dataloader, optimizer, DEVICE)
+        valid_loss, _ = validate_fn(
+            binary_classification_model, valid_dataloader, DEVICE
+        )
+
+        if valid_loss < best_val_loss:
+            best_val_loss = valid_loss
+
+    return best_val_loss
+
+
+def main():
+    clearml.browser_login()
+    task = Task.init(
+        project_name="DBPs_search",
+        task_name="Optuna search",
+        output_uri=False)
+
+    logger = Logger.current_logger()
+
+    study = optuna.create_study(direction='minimize')
+    study.optimize(objective, n_trials=50)
+    trial = study.best_trial
+    for key, value in trial.params.items():
+        message = f"{key}: {value}"
+        logger.report_text(message, level=logging.DEBUG, print_console=False)
+
+    task.close()
+
+
+if __name__ == "__main__":
+    main()
